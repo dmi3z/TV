7a028e355a8ffe9b8b31b160887c6a756fccb11f
Dmitry <dmi3z@ayndex.ru>
2017-07-24 03:21:49 +0300

    All information without refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,34 @@
 import {Component} from '@angular/core';
-import {DataService,Channel} from './shared/index';
+import {DataService,Channel,TvShows} from './shared/index';
 import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
-  
+  templateUrl: './app.component.html'  
 })
+
 export class AppComponent  {
    
    
+    ch_temp:Channel[];
+    tvshows_temp:TvShows[];
     channels:Channel[]=[];
+    tvshows:TvShows[]=[];
     
 
     constructor(private dataService:DataService){}
 
     ngOnInit(){
         
-       this.dataService.getChannels().subscribe(data=>this.channels=data);
+       this.dataService.getChannelsList().subscribe(
+         data=>this.ch_temp=data,
+         error=>error,
+         ()=>this.channels=this.ch_temp); 
+
+        this.dataService.getTvShowsList().subscribe(
+         data=>this.tvshows_temp=data,
+         error=>error,
+         ()=>this.tvshows=this.tvshows_temp); 
                      
     }
  
